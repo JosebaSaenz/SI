850112a538d275ef28d1823afbb02835d1d4fdad
XabiDermit <48221956+XabiDermit@users.noreply.github.com>
2019-04-25 16:13:26 +0200

    .

--- SI/src/Interfaze/DialogErabiltzaileaEzDaExistitzen.java
+++ SI/src/Interfaze/DialogErabiltzaileaEzDaExistitzen.java
@@ -1,42 +1,52 @@
 package Interfaze;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.Toolkit;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
 
-public class DialogErabiltzaileaEzDaExistitzen extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DialogErabiltzaileaEzDaExistitzen frame = new DialogErabiltzaileaEzDaExistitzen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+public class DialogErabiltzaileaEzDaExistitzen extends JDialog {
+	
+	private JLabel jLabel1 = new JLabel();
+	private JButton jButton1 = new JButton();
+	private int idUser;
+	
+	public DialogErabiltzaileaEzDaExistitzen(int pIdUser) {
+		getContentPane().setBackground(Color.DARK_GRAY);
+		setTitle("Arazoa gertatu da");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(DialogErabiltzaileaEzDaExistitzen.class.getResource("/Fitxategiak/dialog_icon.png")));
+		idUser = pIdUser;
+		this.setModal(true);
+		this.setLocation(500,300);
+		this.setSize(new Dimension(450, 160));
+		this.getContentPane().setLayout(null);
+		jLabel1.setForeground(Color.WHITE);
+		jLabel1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		jLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+		jLabel1.setText("'" + idUser + "' erabiltzailea ez da existitzen.");
+		jLabel1.setBounds(new Rectangle(0, 20, 428, 15));
+		jButton1.setForeground(Color.BLACK);
+		jButton1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		jButton1.setBackground(Color.RED);
+		jButton1.setText("Itxi");
+		jButton1.setBounds(new Rectangle(180, 55, 70, 30));
+		this.getContentPane().add(jButton1,null);
+		this.getContentPane().add(jLabel1,null);
+		
+		jButton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
 			}
 		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public DialogErabiltzaileaEzDaExistitzen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
 	}
 
 }
