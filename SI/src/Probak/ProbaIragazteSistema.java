45751884b30dc742cd732872b291314f09af90bf
XabiDermit <48221956+XabiDermit@users.noreply.github.com>
2019-04-29 20:35:08 +0200

    probairagaztesistema

---
+++ SI/src/Probak/ProbaIragazteSistema.java
@@ -0,0 +1,54 @@
+package Probak;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProbaIragazteSistema {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetIragazteSistema() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGomendatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testErabiltzaileaBalorazioaEstimazioa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProduktuaBalorazioaEstimazioa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEzaugarriaBalorazioaEstimazioa() {
+		fail("Not yet implemented");
+	}
+
+}
