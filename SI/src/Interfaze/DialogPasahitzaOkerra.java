3b9b0ac74c020d9a4379e7897591bdb9048cd205
IkerValdelvira <48221928+IkerValdelvira@users.noreply.github.com>
2019-05-05 14:08:07 +0200

    Azken eguneraketa

--- SI/src/Interfaze/DialogPasahitzaOkerra.java
+++ SI/src/Interfaze/DialogPasahitzaOkerra.java
@@ -5,6 +5,7 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
