7fb21587a4dd90ea49a9be4bb1ad1c9970973610
IkerValdelvira <48221928+IkerValdelvira@users.noreply.github.com>
2019-05-05 14:07:49 +0200

    Azken eguneraketa

--- SI/src/Interfaze/DialogEremuakHutsik.java
+++ SI/src/Interfaze/DialogEremuakHutsik.java
@@ -6,6 +6,7 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
